bde051f8de9dc9a8cef4060b51a3aa8be6131b9e
JAHID HASAN <developerjahid@gmail.com>
2023-12-16 14:53:12 +0600

    log in and register form component

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
-
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
 const LogIn = () => {
+     
+     const auth= getAuth(app)
+     console.log(app)
+    const handlerSubmit=event=>{
+        event.preventDefault()
+        const form= event.target;
+        const password= form.password.value;
+        const email=form.email.value;
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then(user=>{
+            const LoggedUser=user.user
+        })
+        .catch(error=>{
+            console.error(error)
+        })
+        
+    }
     return (
         <div>
-            <h1>this is log in </h1>
+           <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+    </div>
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+
+      <form onSubmit={handlerSubmit} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+          <label className="label">
+            <p href="#" className="label-text-alt link link-hover">New to this site? <Link to='/register' style={{color:'blue'}}>Register</Link></p>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,9 +1,66 @@
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import React from 'react';
+import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const auth= getAuth(app)
+    const handlerSignUp=event=>{
+        event.preventDefault()
+        const form= event.target
+        const email= form.email.value;
+        const password= form.password.value;
+
+
+
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(signUpUser=>{
+            const user= signUpUser.user
+            event.target.reset()
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    }
+
     return (
         <div>
-            <h1>this is register</h1>
+         <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Register now!</h1>
+    </div>
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+
+      <form  onSubmit={handlerSignUp} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Your Name</span>
+          </label>
+          <input type="Text" placeholder="Your Name" name='name' className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" name='email' className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" name='password' className="input input-bordered" required />
+          <label className="label">
+            <p href="#" className="label-text-alt link link-hover">alredy have an account? <Link to='/login' style={{color:'blue'}}>Log in</Link></p>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
