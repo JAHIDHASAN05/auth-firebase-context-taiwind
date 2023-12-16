98c5d9c70321be8d6829cf9c9e2fce55bdcca7ae
JAHID HASAN <developerjahid@gmail.com>
2023-12-16 13:36:46 +0600

    first commit navbar with react router done

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+          
+            <div className="navbar bg-primary text-primary-content">
+                <button className="btn btn-ghost text-xl">daisyUI</button>
+                <Link to={'/'} className="btn btn-ghost text-xl">Home</Link>
+                <Link to={'/login'} className="btn btn-ghost text-xl">Log In</Link>
+                <Link to={'/register'} className="btn btn-ghost text-xl">Register</Link>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHEz9DbTzLz3BkOJez2jcfDzuywhrfzsE",
+  authDomain: "auth-firebase-context-ta-fc9e4.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-fc9e4",
+  storageBucket: "auth-firebase-context-ta-fc9e4.appspot.com",
+  messagingSenderId: "727052330019",
+  appId: "1:727052330019:web:97cd13a6ac983e7513fd96"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
